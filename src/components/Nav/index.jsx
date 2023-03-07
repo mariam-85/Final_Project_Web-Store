1b5bedfefe5755d3c639fb797e8ba49cb01f8dba
mariam-85 <mariam.lofink85@gmail.com>
2023-03-07 17:15:48 +0100

    hash link added

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,31 +2,32 @@ import React from 'react'
 import cart_img from './media/cart_pic.png'
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+// import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { useSelector } from 'react-redux'
 import { MenuOutlined } from '@ant-design/icons'
+import { HashLink } from 'react-router-hash-link';
 
 export default function Nav({ menuActive, setMenuActive }) {
 
 const cart = useSelector(state => state.cart);
 
-const cart_count = cart.reduce((total,item) => total + item.count, 0 )
+const cart_count = cart.reduce((total, item) => total + item.count, 0 )
 
   return (
     <nav className={style.nav_block}>
       <div className={[ style.nav_menu, menuActive ? style.active : '' ].join(' ')}>
-        <Link to='categories'>
+        <HashLink to='categories'>
           <p>Catalogs</p>
-        </Link>
-        <AnchorLink href='#sale_block'>
+        </HashLink>
+        <HashLink smooth to='/#sale_block'>
           <p>Coupon</p>
-        </AnchorLink>
-        <AnchorLink href='#discount'>
+        </HashLink>
+        <HashLink smooth to='/#discount'>
           <p>Discount</p>
-        </AnchorLink>
-        <AnchorLink  href='#contacts'>
+        </HashLink>
+        <HashLink  smooth to='/#contacts'>
           <p>Contact</p>
-        </AnchorLink>
+        </HashLink>
         
         <div className={style.nav_cart}>
           <Link to='cart'>
@@ -34,9 +35,9 @@ const cart_count = cart.reduce((total,item) => total + item.count, 0 )
               {
                 cart.length !== 0 ? <p>{ cart_count }</p> : ''
               }
-          </Link>
-          </div>
-          </div>
+        </Link>
+        </div>
+      </div>
           <MenuOutlined className={style.menu_icon} onClick={() => setMenuActive(menuActive ? false : true)}/>
     </nav>
   )
--- src/requests/category.js
+++ src/requests/category.js
@@ -1,6 +1,3 @@
-// на бекенде добавила возможность делать запрос на отдельную категорию 
-// по ссылке http://localhost:3333/categories/info/1
-
 import { loadCategoryById } from "../store/reducers/category.js"; 
 
 export const getCategory = (id) => {
